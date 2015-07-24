31bdd2fcafda576fe1ea215f7ce9e4c552a28989
shug3502 <jum.harrison@yahoo.co.uk>
2015-07-24 15:21:02 +0100

    First working version of naive CNN faces implementation

---
+++ Naive_Faces_CNN_wrapper.m
@@ -0,0 +1,170 @@
+function Naive_Faces_CNN_wrapper(entries_of_interest,imdb,varargin)
+% Based on Part 4 of the VGG CNN practical
+%Make sure in correct directory
+%setup ;
+
+% -------------------------------------------------------------------------
+% Part 4.1: prepare the data
+% -------------------------------------------------------------------------
+if isempty(imdb)
+% Load dataset
+fprintf('Loading dataset\n');
+[imdb.images.data, imdb.images.label, imdb.images.id] = crop_faces(entries_of_interest) ;
+rr = rand(1,size(imdb.images.data,3));
+imdb.images.set = 1*(rr<0.8)+2*(rr>=0.8);
+end
+% % Visualize some of the data 
+figure(1) ; clf ; colormap gray ;
+subplot(1,2,1) ;
+vl_imarraysc(imdb.images.data(:,:,imdb.images.label==1 & imdb.images.set==1)) ;
+axis image off ;
+title('training examples for ''angelman''') ;
+
+subplot(1,2,2) ;
+vl_imarraysc(imdb.images.data(:,:,imdb.images.label==1 & imdb.images.set==2)) ;
+axis image off ;
+title('validation examples for ''angelman''') ;
+
+% -------------------------------------------------------------------------
+% Part 4.2: initialize a CNN architecture
+% -------------------------------------------------------------------------
+fprintf('Initializing CNN\n');
+net = initializeNaiveFacesCNN() ;
+
+% -------------------------------------------------------------------------
+% Part 4.3: train and evaluate the CNN
+% -------------------------------------------------------------------------
+
+trainOpts.batchSize = 100 ;
+trainOpts.numEpochs = 150 ;
+trainOpts.continue = true ;
+trainOpts.useGpu = false ;
+trainOpts.learningRate = 0.001 ;
+trainOpts.expDir = 'Facial_gestalt/naive-experiment' ;
+trainOpts = vl_argparse(trainOpts, varargin);
+
+% Take the average image out
+imageMean = mean(imdb.images.data(:)) ;
+%subtract average
+imdb.images.data = imdb.images.data - imageMean ;
+
+% Convert to a GPU array if needed
+if trainOpts.useGpu
+    error('gpu not yet supported');
+  imdb.images.data = gpuArray(imdb.images.data) ;
+end
+
+fprintf('Training CNN\n');
+% Call training function in MatConvNet
+[net,info] = cnn_train_adapted(net, imdb, @getBatch, 'batchSize',trainOpts.batchSize,...
+    'numEpochs', trainOpts.numEpochs, 'continue', trainOpts.continue,...
+    'learningRate', trainOpts.learningRate,...
+    'expDir', trainOpts.expDir) ; 
+
+% Move the CNN back to the CPU if it was trained on the GPU
+if trainOpts.useGpu
+  error('gpu not yet supported');  
+  net = vl_simplenn_move(net, 'cpu') ;
+end
+
+% Save the result for later use
+net.layers(end) = [] ;
+net.imageMean = imageMean ;
+save('Facial_gestalt/naive-experiment/NaiveFacesCNN.mat', '-struct', 'net') ;
+
+% -------------------------------------------------------------------------
+% Part 4.4: visualize the learned filters
+% -------------------------------------------------------------------------
+
+figure(2) ; clf ; colormap gray ;
+vl_imarraysc(squeeze(net.layers{1}.filters),'spacing',2)
+axis equal ; title('filters in the first layer') ;
+
+% -------------------------------------------------------------------------
+% Part 4.5: apply the model
+% -------------------------------------------------------------------------
+
+% Load the CNN learned before
+%net = load('Facial_gestalt/naive-experiment/NaiveFacesCNN.mat') ;
+%net = load('data/chars-experiment/charscnn-jit.mat') ;
+
+%Now apply the model to some kind of example test image/data set
+
+%If initialis all working, then can also try applying to extension with jitter 
+% % -------------------------------------------------------------------------
+% % Part 4.6: train with jitter
+% % -------------------------------------------------------------------------
+% 
+% trainOpts.batchSize = 100 ;
+% trainOpts.numEpochs = 15 ;
+% trainOpts.continue = true ;
+% trainOpts.learningRate = 0.001 ;
+% trainOpts.expDir = 'tutorial/chars-jit-experiment' ;
+% 
+% % Initlialize a new network
+% net = initializeCharacterCNN() ;
+% 
+% % Call training function in MatConvNet
+% [net,info] = cnn_train_adapted(net, imdb, @getBatchWithJitter,  'batchSize',trainOpts.batchSize,...
+%     'numEpochs', trainOpts.numEpochs, 'continue', trainOpts.continue,...
+%     'learningRate', trainOpts.learningRate,...
+%     'expDir', trainOpts.expDir) ;
+% 
+% % Move the CNN back to CPU if it was trained on GPU
+% if trainOpts.useGpu
+%   net = vl_simplenn_move(net, 'cpu') ;
+% end
+% 
+% % Save the result for later use
+% net.layers(end) = [] ;
+% net.imageMean = imageMean ;
+% save('tutorial/chars-experiment/charscnn-jit.mat', '-struct', 'net') ;
+% 
+% % Visualize the results on the sentence
+% figure(4) ; clf ;
+% decodeCharacters(net, imdb, im, vl_simplenn(net, im)) ;
+
+end
+
+% --------------------------------------------------------------------
+function [im, labels] = getBatch(imdb, batch)
+% --------------------------------------------------------------------
+im = imdb.images.data(:,:,batch);
+im = 256 * reshape(im, 125, 125, 1, []) ;
+labels = imdb.images.label(1,batch);
+end
+% --------------------------------------------------------------------
+% function [im, labels] = getBatchWithJitter(imdb, batch)
+% % --------------------------------------------------------------------
+% im = imdb.images.data(:,:,batch) ;
+% labels = imdb.images.label(1,batch) ;
+% 
+% n = numel(batch) ;
+% train = find(imdb.images.set == 1) ;
+% 
+% sel = randperm(numel(train), n) ;
+% im1 = imdb.images.data(:,:,sel) ;
+% 
+% sel = randperm(numel(train), n) ;
+% im2 = imdb.images.data(:,:,sel) ;
+% 
+% ctx = [im1 im2] ;
+% ctx(:,17:48,:) = min(ctx(:,17:48,:), im) ;
+% 
+% dx = randi(11) - 6 ;
+% im = ctx(:,(17:48)+dx,:) ;
+% sx = (17:48) + dx ;
+% 
+% dy = randi(5) - 2 ;
+% sy = max(1, min(32, (1:32) + dy)) ;
+% 
+% im = ctx(sy,sx,:) ;
+% 
+% % Visualize the batch:
+% % figure(100) ; clf ;
+% % vl_imarraysc(im) ;
+% 
+% im = 256 * reshape(im, 32, 32, 1, []) ;
+% end
+% 
+% 
--- crop_faces.m
+++ crop_faces.m
@@ -1,31 +1,40 @@
-function input_data = crop_faces(entries_of_interest,num_to_load)
+function [input_data, input_labels, input_id] = crop_faces(entries_of_interest)
 %function to crop and rotate faces
 close all
 tic;
-if nargin ~=2
+if nargin <1
     load('entries_of_interest_ma.mat');
-    num_to_load = 44;
 end
-scale = [145, 131];
-input_data = zeros(scale(1)*scale(2),length(entries_of_interest));
+
+scale = [125,125]; %[145, 131];
+input_data = zeros(scale(1),scale(2),length(entries_of_interest));
+input_labels = zeros(1, length(entries_of_interest));
 for i=1:length(entries_of_interest)
-num_to_load = i
-current_im = imread(entries_of_interest(num_to_load).imagePath);
-%cropped_im = imcrop(current_im,entries_of_interest(num_to_load).face_coordinates);
+current_im = imread(entries_of_interest(i).imagePath);
+cropped_im = imcrop(current_im,entries_of_interest(i).face_coordinates);
 
 %now rescale and rotate according to canonical transform
-%J = imresize(cropped_im, [224,224]); 
-%imshow(J);
-
- if num_to_load==109
-     figure(5), imshow(current_im)
- end
-% hold on
-% plot(entries_of_interest(num_to_load).ground_truth_points(:,1),entries_of_interest(num_to_load).ground_truth_points(:,2),'bo')
-figure
-[ proc_image ] = pre_proc_test( current_im, entries_of_interest(num_to_load).ground_truth_points, 1);
-s = size(proc_image)
-input_data(:,i) = reshape(proc_image,s(1)*s(2),1);
-
+J = imresize(cropped_im, scale); 
+% figure(1)
+% imshow(J);
+J = rgb2gray(J);
+%  if num_to_load==109
+%      figure(5), imshow(current_im)
+%  end
+ 
+% figure;
+% [ proc_image ] = pre_proc_test( current_im, entries_of_interest(num_to_load).ground_truth_points, 0);
+% s = size(proc_image);
+input_data(:,:,i) = reshape(J,scale(1),scale(2));
+input_labels(i) = 1*strcmp(entries_of_interest(i).syndrome,'Angelman') ...
+    + 2*strcmp(entries_of_interest(i).syndrome,'Apert') ...
+    + 3*strcmp(entries_of_interest(i).syndrome,'Cornelia_de_Lange_Syndrome')...
+    + 4*strcmp(entries_of_interest(i).syndrome,'Down')...
+    + 5*strcmp(entries_of_interest(i).syndrome,'FragileX')...
+    + 6*strcmp(entries_of_interest(i).syndrome,'Progeria')...
+    + 7*strcmp(entries_of_interest(i).syndrome,'Treacher-Collins')...
+    + 8*strcmp(entries_of_interest(i).syndrome,'Williams')...
+    + 9*strcmp(entries_of_interest(i).syndrome,'Control');
 end
+input_id = 1:length(entries_of_interest); %entries_of_interest(i).id;
 toc
